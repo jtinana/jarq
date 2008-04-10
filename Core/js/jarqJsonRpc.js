7330d30a3390acc615bf0163ef567fb039991cea
jtinana <jtinana@e1bfe95d-9d49-0410-a439-836f03a291c9>
2008-04-10 09:02:32 +0000

    Introduccion en fichero JS de las funciones para el displayTag de marcado de lineas.

--- Core/js/jarqJsonRpc.js
+++ Core/js/jarqJsonRpc.js
@@ -7,26 +7,26 @@
 // callBackFunction -> Funcion JavaScript para utilizar como CallBack. Esta funcion debe recibir dos argumentos, el primero sera el
 //resultado, y el segundo la excepcion, que sea nula en caso de ir correcto. Si se indica un valor, la funcion jasonCall no
 // devuelve nada.
-function jsonEntityArrayCall( bridgeName, methodName, args, callBackFunction )
+function jsonEntityArrayCall(bridgeName, methodName, args, callBackFunction)
 {
     try
     {
         var jsonrpc = new JSONRpcClient("/GI_Proyectos/JSON-RPC");
-        var expression =  'jsonrpc.' + bridgeName + '.' + methodName + '(';
+        var expression = 'jsonrpc.' + bridgeName + '.' + methodName + '(';
 
         //si se ha indicado Callback function la ponemos como primer argumento.
-        if( callBackFunction != null && callBackFunction.length > 0 )
+        if (callBackFunction != null && callBackFunction.length > 0)
         {
             expression += callBackFunction + ', ';
         }
 
         //Recorremos los argumentos para pasarselos al metodo.
-        if( args != null )
+        if (args != null)
         {
-            for( var i=0; i<args.length; i++ )
+            for (var i = 0; i < args.length; i++)
             {
                 expression += args[i];
-                if( args.length > (i+1) )
+                if (args.length > (i + 1))
                 {
                     expression += ',';
                 }
@@ -37,7 +37,7 @@ function jsonEntityArrayCall( bridgeName, methodName, args, callBackFunction )
 
         var result = eval(expression);
 
-        if( callBackFunction == null || callBackFunction.length == 0 )
+        if (callBackFunction == null || callBackFunction.length == 0)
         {
             var evaluatedResult = eval("(" + toJSON(result.list) + ")");
             return evaluatedResult;
@@ -57,111 +57,128 @@ function jsonEntityArrayCall( bridgeName, methodName, args, callBackFunction )
 
 
 //Oculta un elemento por ID.
-function f_hideElementById( elementId )
+function f_hideElementById(elementId)
 {
-    if( document.getElementById( elementId ) )
+    if (document.getElementById(elementId))
     {
-        document.getElementById( elementId ).style.visibility = 'hidden';
+        document.getElementById(elementId).style.visibility = 'hidden';
     }
 }
 
 //Hace visible un elemento indicado por id.
-function f_showElement( elementId )
+function f_showElement(elementId)
 {
-    var elementToShow = document.getElementById( elementId );
+    var elementToShow = document.getElementById(elementId);
     elementToShow.style.visibility = 'visible';
 }
 
 //Rellena el combo con el ID 'comboId' utilizando el array 'jsonPlainObjectArray'. El parametro idField sera el campo
 // del bean a utilizar como value del combo, y el textField el campo del bean a poner como label.
-function f_fillUpCombo(comboId, jsonPlainObjectArray, idField, textField) {
-	var selectElement= document.getElementById(comboId);
-	var index= selectElement.options.length;
-	for (var i= 0; i < jsonPlainObjectArray.length; i++) {
-		selectElement.options[i + index]= new Option(jsonPlainObjectArray[i][textField], jsonPlainObjectArray[i][idField]);
-	}
+function f_fillUpCombo(comboId, jsonPlainObjectArray, idField, textField)
+{
+    var selectElement = document.getElementById(comboId);
+    var index = selectElement.options.length;
+    for (var i = 0; i < jsonPlainObjectArray.length; i++)
+    {
+        selectElement.options[i + index] = new Option(jsonPlainObjectArray[i][textField], jsonPlainObjectArray[i][idField]);
+    }
 }
 //Elimina el elemento de la posicion seleccionada del combo
-function f_removeElementCombo(comboId, index) {
-	var selectElement = document.getElementById(comboId);
-	selectElement.options.remove(index);
+function f_removeElementCombo(comboId, index)
+{
+    var selectElement = document.getElementById(comboId);
+    selectElement.options.remove(index);
 }
 //Vacia el combo indicado por ID.
-function f_emptyCombo(comboId) {
-	var selectElement = document.getElementById(comboId);
-	for (var i=0; i<selectElement.options.length; i++) {
-		selectElement.options[i] = null;
-	}
+function f_emptyCombo(comboId)
+{
+    var selectElement = document.getElementById(comboId);
+    for (var i = 0; i < selectElement.options.length; i++)
+    {
+        selectElement.options[i] = null;
+    }
 }
 
 //Realiza un trim del String pasado como parametro.
 function trim(cadena)
 {
-	for(var i=0; i<cadena.length; )
-	{
-		if(cadena.charAt(i)==" ")
-			cadena=cadena.substring(i+1, cadena.length);
-		else
-			break;
-	}
+    for (var i = 0; i < cadena.length;)
+    {
+        if (cadena.charAt(i) == " ")
+            cadena = cadena.substring(i + 1, cadena.length);
+        else
+            break;
+    }
 
-	for(var i=cadena.length-1; i>=0; i=cadena.length-1)
-	{
-		if(cadena.charAt(i)==" ")
-			cadena=cadena.substring(0,i);
-		else
-			break;
-	}
+    for (var i = cadena.length - 1; i >= 0; i = cadena.length - 1)
+    {
+        if (cadena.charAt(i) == " ")
+            cadena = cadena.substring(0, i);
+        else
+            break;
+    }
 
-	return cadena;
+    return cadena;
 }
 
 //Obtiene el valor seleccionado actualmente del combo indicado por ID.
-function f_obtenerValorComboPorId(comboId) {
-	if (comboId== null) {
-		throw "No se puede indicar un ID a null";
-	}
-	var comboElement = document.getElementById( comboId );
-	if (comboElement != null) {
-//		alert('comboElement.selectedIndex= ' + comboElement.selectedIndex);
-		if (comboElement.selectedIndex > -1) {
-			var comboValue= comboElement.options[comboElement.selectedIndex].value;
+function f_obtenerValorComboPorId(comboId)
+{
+    if (comboId == null)
+    {
+        throw "No se puede indicar un ID a null";
+    }
+    var comboElement = document.getElementById(comboId);
+    if (comboElement != null)
+    {
+        //		alert('comboElement.selectedIndex= ' + comboElement.selectedIndex);
+        if (comboElement.selectedIndex > -1)
+        {
+            var comboValue = comboElement.options[comboElement.selectedIndex].value;
 //			alert('comboValue= ' + comboValue);
-			if (comboValue != '') {
-				return comboElement.options[comboElement.selectedIndex].value;
-			} else {
-				return null;
-			}
-		} else {
-			return null;
-		}
-	} else {
-//		alert("Ha indicado un id que no existe: " + comboId);
-		return null;
-	}
+            if (comboValue != '')
+            {
+                return comboElement.options[comboElement.selectedIndex].value;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        else
+        {
+            return null;
+        }
+    }
+    else
+    {
+        //		alert("Ha indicado un id que no existe: " + comboId);
+        return null;
+    }
 }
 
 //Obtiene el numero de elementos que contiene el combo actualmente.
-function f_comboLenght(idCombo) {
-	var combo = document.getElementById(idCombo);
-	return combo.options.length;
+function f_comboLenght(idCombo)
+{
+    var combo = document.getElementById(idCombo);
+    return combo.options.length;
 }
 
 //Llamada a JSON para la obtencion de una entidad unicamente.
-function jsonEntityCall( bridgeName, methodName, args )
+function jsonEntityCall(bridgeName, methodName, args)
 {
     try
     {
         var jsonrpc = new JSONRpcClient("/GI_Proyectos/JSON-RPC");
-        var expression =  'jsonrpc.' + bridgeName + '.' + methodName + '(';
+        var expression = 'jsonrpc.' + bridgeName + '.' + methodName + '(';
 
         //Recorremos los argumentos para pasarselos al metodo.
-        if( args != null )
+        if (args != null)
         {
-            for( var i=0; i<args.length; i++ )
+            for (var i = 0; i < args.length; i++)
             {
                 expression += args[i];
-                if( args.length > (i+1) )
+                if (args.length > (i + 1))
                 {
                     expression += ',';
                 }
@@ -172,7 +189,7 @@ function jsonEntityCall( bridgeName, methodName, args )
 
         var result = eval(expression);
 
-        var evaluatedResult = eval( '(' + toJSON(result) + ')');
+        var evaluatedResult = eval('(' + toJSON(result) + ')');
         return evaluatedResult;
     }
     catch (e)
@@ -183,14 +200,14 @@ function jsonEntityCall( bridgeName, methodName, args )
 }
 
 //Obtiene el valor del atributo indicado de la entidad que se le pasa como parametro.
-function f_obtenerAttEntidad( entidad, atributo )
+function f_obtenerAttEntidad(entidad, atributo)
 {
-    if( entidad == null )
+    if (entidad == null)
     {
         throw "Debe pasarse una entidad como parametro";
     }
 
-    if( atributo == null )
+    if (atributo == null)
     {
         throw "Debe pasarse un atributo como parametro";
     }
@@ -199,17 +216,21 @@ function f_obtenerAttEntidad( entidad, atributo )
 }
 
 //Establece en valor vacio, o -1 del combo indicado por id.Si el combo no posee un option con valor '' o -1 no hace nada.
-function f_resetCombo(comboId) {
-	if (document.getElementById(comboId)) {
-		var selectElement= document.getElementById(comboId);
-		for (var indexOption=0; indexOption < selectElement.options.length; indexOption++) {
-			selectElement.options[selectElement.selectedIndex].selected = false;
-			if (selectElement.options[indexOption].value== '' ||
-			selectElement.options[indexOption].value== '-1' ||
-			selectElement.options[indexOption].value== -1) {
-				//Establecemos ese valor como seleccionado y quitamos el que esta.
-				selectElement.options[indexOption].selected = true;
-			}
-		}
-	}
+function f_resetCombo(comboId)
+{
+    if (document.getElementById(comboId))
+    {
+        var selectElement = document.getElementById(comboId);
+        for (var indexOption = 0; indexOption < selectElement.options.length; indexOption++)
+        {
+            selectElement.options[selectElement.selectedIndex].selected = false;
+            if (selectElement.options[indexOption].value == '' ||
+                selectElement.options[indexOption].value == '-1' ||
+                selectElement.options[indexOption].value == -1)
+            {
+                //Establecemos ese valor como seleccionado y quitamos el que esta.
+                selectElement.options[indexOption].selected = true;
+            }
+        }
+    }
 }
